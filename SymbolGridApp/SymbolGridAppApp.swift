cbc09076568edf719b4103f6bf35fb55d9a5c544
Eric <ebagnaro@gmail.com>
2023-11-21 13:52:52 +0100

    Initial Commit

---
+++ SymbolGridApp/SymbolGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridAppApp.swift
+//  SymbolGridApp
+//
+//  Created by Eric on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
